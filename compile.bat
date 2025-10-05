9d9eb07f968dc83cc60f0f769d79f8814513241a
morgana-x <89588301+morgana-x@users.noreply.github.com>
2025-10-05 16:10:24 +1100

    Restructure project

--- compile.bat
+++ compile.bat
@@ -1,3 +1,6 @@
 @echo off
-lin_compiler source %RELOADEDIIMODS%\dr1.script.demoenglish\FEmulator\WAD\dr1_data_us.wad\Dr1\data\us\script
+tool\lin_compiler source dr1.script.demoenglish\FEmulator\WAD\dr1_data_us.wad\Dr1\data\us\script
+tool\DrDebateTool dr1.script.demoenglish\FEmulator\WAD\dr1_data_us.wad\Dr1\data\us\bin\nonstop_10_001.txt
+tool\DrDebateTool dr1.script.demoenglish\FEmulator\WAD\dr1_data_us.wad\Dr1\data\us\bin\nonstop_10_002.txt
+robocopy dr1.script.demoenglish %RELOADEDIIMODS%\dr1.script.demoenglish /E
 pause
